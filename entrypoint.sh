c53849fd2c658e4bdc244104e2699a9254cadc3d
Stanislas Castaybert <perso@stan1712.com>
2025-10-27 01:19:54 +0100

    🚚 Renamed sbdx path to container to ensure consistency with Pterodactyl

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+
+export COREPACK_HOME="/tmp/corepack"
 corepack enable
 
 echo "$ cd /home/container"
